d1057a8ac61f190600b69338b1874e65ad4bdbb1
Jamie Dignam <jamie@101gamers.com>
2016-09-10 20:06:53 +0100

    Add our community to the footer!
    Minus the 'code changes credits' this time.

--- footer.php
+++ footer.php
@@ -5,8 +5,9 @@ else echo '<a href="#" onclick="enableMobileLayout(-1); return false;" rel="nofo
 <br>
 <br>
 <?php $bucket = "footer"; include("./lib/pluginloader.php");?>
-Powered by <a href="http://abxd.dirbaio.net/">AcmlmBoard XD</a><br />
+Powered by <a href="http://github.com/Kawaii-Volt32/101Gamers-ABXD">AcmlmBoard XD</a> v3.0<br />
 By Dirbaio, xfix, Kawa, StapleButter, Nina, et al<br />
+<a href="https://www.101gamers.com/" alt="101Gamers">101Gamers</a><br />
 AcmlmBoard &copy; Jean-Fran&ccedil;ois Lapointe<br />
 <?php print __("<!-- English translation by The ABXD Team -->")?>
 
